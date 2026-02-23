af4bddebfc4b7c85bef2b5edff88feab0170842f
Chris Sha <shashuo0104@gmail.com>
2026-02-23 17:52:47 -0500

    initial commit

---
+++ static/js/index.js
@@ -0,0 +1,23 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+    });
+
+    var options = {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      loop: true,
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 3000,
+    }
+
+    // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    bulmaSlider.attach();
+});
