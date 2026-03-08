6fc35d3690031798d3728e0884448c2bc31564ba
Chris Sha <shashuo0104@gmail.com>
2026-03-08 15:55:53 -0400

    update title

--- bbbb.sh
+++ bbbb.sh
@@ -4,10 +4,10 @@ gs -sDEVICE=pdfwrite \
    -dCompatibilityLevel=1.4 \
    -dPDFSETTINGS=/ebook \
    -dNOPAUSE -dQUIET -dBATCH \
-   -sOutputFile=/Users/shuo/Desktop/rrl/clean/figures/pdf/${name}.pdf \
-   /Users/shuo/Desktop/rrl/clean/figures/pdf_large/${name}.pdf
+   -sOutputFile=/Users/shuo/Documents/papers/26Feb_residual_copilot/Release/clean/figures/pdf/${name}.pdf \
+   /Users/shuo/Documents/papers/26Feb_residual_copilot/Release/clean/figures/pdf_large/${name}.pdf
 
-input="/Users/shuo/Desktop/rrl/clean/figures/pdf_large/${name}.pdf"
+input="/Users/shuo/Documents/papers/26Feb_residual_copilot/Release/clean/figures/pdf/${name}.pdf"
 outdir="/Users/shuo/projects/residual-copilot.github.io/files/images"
 
 # PDF -> PNG (2000px wide)
@@ -15,3 +15,5 @@ sips -s format png --resampleWidth 2000 "$input" --out "${outdir}/${name}.png"
 
 # PDF -> JPG (2000px wide, 85% quality)
 sips -s format jpeg -s formatOptions 85 --resampleWidth 2000 "$input" --out "${outdir}/${name}.jpg"
+
+
